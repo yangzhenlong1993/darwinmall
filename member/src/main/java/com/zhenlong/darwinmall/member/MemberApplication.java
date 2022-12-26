0e2fb29745a420b5dad12fbbda62f7daa24015c4
zhenlong.yang <yangzhenlong1993@gmail.com>
2022-12-27 00:18:14 +0930

    initialize the project

---
+++ member/src/main/java/com/zhenlong/darwinmall/member/MemberApplication.java
@@ -0,0 +1,13 @@
+package com.zhenlong.darwinmall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+
+}
